080012cf6596e070a21ada394669346d7e580fc7
kunesv <vaclavkunes@sprinters.cz>
2017-02-23 01:12:17 +0100

    updating some tests

--- src/test/java/net/stuha/messages/ImageControllerTest.java
+++ src/test/java/net/stuha/messages/ImageControllerTest.java
@@ -2,19 +2,18 @@ package net.stuha.messages;
 
 import net.stuha.AbstractControllerTest;
 import org.junit.Test;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class ImageControllerTest extends AbstractControllerTest {
 
-    private MockMvc mockMvc;
-
     @Test
     public void find() throws Exception {
-        mockMvc.perform(get("/image/aaaabbbb-aaaa-bbbb-aaaabbbbcccc"))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/image/aaaabbbb-aaaa-bbbb-aaaabbbbcccc")
+                .header("userId", "eeee1111-bbbb-cccc-eeee-ffffffffffff")
+                .header("token", "I5aDtwfM1OrBtgYlsoM7QvOxhqRWDSNzDqxXNWjlzacgnGxaN6EExbRK4IF13JBZp8qvcJDRjaHDxnrlhOWNpnXddSyFUSyqC2JVDDvbwCmlNM3rHqqM2OThuV03X2EP")
+        ).andExpect(status().isOk());
     }
 
 }
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,13 +1,15 @@
 CREATE TABLE conversation (
   id    UUID NOT NULL,
-  title CHARACTER VARYING(255)
+  title CHARACTER VARYING(255),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE icon (
   id      UUID NOT NULL,
   alt     CHARACTER VARYING(4),
   path    CHARACTER VARYING(8),
-  user_id UUID NOT NULL
+  user_id UUID NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE image (
@@ -15,7 +17,8 @@ CREATE TABLE image (
   image      BYTEA                  NOT NULL,
   message_id UUID                   NOT NULL,
   name       CHARACTER VARYING(255) NOT NULL,
-  thumbnail  BYTEA                  NOT NULL
+  thumbnail  BYTEA                  NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE message (
@@ -27,7 +30,8 @@ CREATE TABLE message (
   reply_to        UUID,
   rough           TEXT,
   user_id         UUID                   NOT NULL,
-  user_name       CHARACTER VARYING(255) NOT NULL
+  user_name       CHARACTER VARYING(255) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE token (
@@ -35,47 +39,27 @@ CREATE TABLE token (
   auto_revalidate BOOLEAN,
   created_on      TIMESTAMP,
   token           CHARACTER VARYING(255),
-  user_id         UUID NOT NULL
+  user_id         UUID NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE user_credentials (
   id       UUID NOT NULL,
   password CHARACTER VARYING(255),
-  username CHARACTER VARYING(255)
+  username CHARACTER VARYING(255),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE users (
   id       UUID NOT NULL,
   name     CHARACTER VARYING(255),
-  username CHARACTER VARYING(255)
+  username CHARACTER VARYING(255),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE user_conversation (
   id              UUID NOT NULL,
   user_id         UUID NOT NULL,
-  conversation_id UUID NOT NULL
+  conversation_id UUID NOT NULL,
+  PRIMARY KEY (id)
 );
-
-ALTER TABLE ONLY conversation
-  ADD CONSTRAINT conversation_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY icon
-  ADD CONSTRAINT icon_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY image
-  ADD CONSTRAINT image_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY message
-  ADD CONSTRAINT message_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY token
-  ADD CONSTRAINT token_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY user_credentials
-  ADD CONSTRAINT user_credentials_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY users
-  ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY user_conversation
-  ADD CONSTRAINT user_conversation_pkey PRIMARY KEY (id);
