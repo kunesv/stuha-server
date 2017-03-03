eebc3332e0959b262e9fb50d6c43496c3453a774
kunesv <vaclavkunes@sprinters.cz>
2017-03-03 01:16:23 +0100

    Adding Cache Control

--- src/main/java/net/stuha/Application.java
+++ src/main/java/net/stuha/Application.java
@@ -1,14 +1,16 @@
 package net.stuha;
 
 import net.stuha.security.AuthorizationInterceptor;
-import net.stuha.security.TokenWriteInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 /**
  * Executable
@@ -27,14 +29,23 @@ public class Application extends WebMvcConfigurerAdapter {
     }
 
     @Bean
-    public TokenWriteInterceptor tokenWriteInterceptor() {
-        return new TokenWriteInterceptor();
+    public WebContentInterceptor webContentInterceptor() {
+        WebContentInterceptor webContentInterceptor = new WebContentInterceptor();
+        webContentInterceptor.setCacheControl(CacheControl.noCache());
+        webContentInterceptor.setCacheSeconds(0);
+        return webContentInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authorizationInterceptor()).addPathPatterns("/**").excludePathPatterns("/login");
-        registry.addInterceptor(tokenWriteInterceptor());
+        registry.addInterceptor(webContentInterceptor()).addPathPatterns("/**");
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").setCacheControl(CacheControl.noCache()).setCachePeriod(0);
     }
 
 }
